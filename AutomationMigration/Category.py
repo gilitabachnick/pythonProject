09e734bfc9568fbb2ae8f0a6f5991af90a848923
gilitabachnick <109959260+gilitabachnick@users.noreply.github.com>
2024-12-12 13:02:47 +0200

    Delete AutomationMigration directory

--- AutomationMigration/Category.py
+++
@@ -1,8 +0,0 @@
-from KalturaClient import *
-
-config = KalturaConfiguration(PARTNER_ID)
-config.serviceUrl = "https://admin.kaltura.com/"
-client = KalturaClient(config)
-filter = KalturaCategoryFilter()
-pager = None
-result = client.category.list(filter, pager)
